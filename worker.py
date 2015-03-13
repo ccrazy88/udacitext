2f80e1471cde0bd4832f7273d5ac799726686ea4
Chrisna Aing <chrisna@chrisna.org>
2015-03-13 12:58:32 -0700

    Change apostrophes to quotation marks.

--- worker.py
+++ worker.py
@@ -3,13 +3,13 @@ import urlparse
 from redis import Redis
 from rq import Worker, Queue, Connection
 
-listen = ['high', 'default', 'low']
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-urlparse.uses_netloc.append('redis')
+listen = ["high", "default", "low"]
+urlparse.uses_netloc.append("redis")
+redis_url = os.getenv("REDISTOGO_URL", "redis://localhost:6379")
 url = urlparse.urlparse(redis_url)
 conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with Connection(conn):
         worker = Worker(map(Queue, listen))
         worker.work()

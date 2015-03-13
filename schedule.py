ab458afef6ed796430c731813201cbb6f86d0c3e
Chrisna Aing <chrisna@chrisna.org>
2015-03-13 02:39:09 -0700

    Fix Redis connection in scheduler.

--- schedule.py
+++ schedule.py
@@ -1,7 +1,18 @@
 from redis import Redis
 from rq import Queue
 
+import os
+import urlparse
+
 from text import udacitext
 
-queue = Queue(connection=Redis())
+redis_url = os.getenv('REDISTOGO_URL')
+if not redis_url:
+    raise RuntimeError('Set up Redis To Go first.')
+
+urlparse.uses_netloc.append('redis')
+url = urlparse.urlparse(redis_url)
+conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
+
+queue = Queue(connection=conn)
 job = queue.enqueue(udacitext)

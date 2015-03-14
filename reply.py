370c83ed9c3cd0a9d8998e752fcc9f8fc3a01d85
Chrisna Aing <chrisna@chrisna.org>
2015-03-13 17:37:38 -0700

    Fix for Twilio TwiML.

--- reply.py
+++ reply.py
@@ -1,8 +1,7 @@
 from flask import Flask, request
-from twilio import TwilioRestException
-from twilio.rest import TwilioRestClient
 import psycopg2
 import os
+import twilio.twiml
 import urlparse
 
 
@@ -22,17 +21,10 @@ POSTGRES_KWARGS = {
     "port": url.port
 }
 
-# Twilio constants
-TWILIO_SID = os.environ["TWILIO_SID"]
-TWILIO_TOKEN = os.environ["TWILIO_TOKEN"]
-TWILIO_NUMBER = os.environ["TWILIO_NUMBER"]
-
 
 @app.route("/", methods=['GET', 'POST'])
 def receive():
-    phone_number = request.values.get('From', None)
     from_body = request.values.get('Body', None)
-
     if from_body and from_body.strip().upper() in keywords:
         with psycopg2.connect(**POSTGRES_KWARGS) as connection:
             messages = execute_query(
@@ -44,31 +36,19 @@ def receive():
                                                 end_timestamp;
                 """
             )
-            for message in messages:
-                body = message[0]
-                try:
-                    client = TwilioRestClient(TWILIO_SID, TWILIO_TOKEN)
-                    message = client.messages.create(to=phone_number,
-                                                     from_=TWILIO_NUMBER,
-                                                     body=body)
-                except TwilioRestException as error:
-                    insert_error(connection, error, phone_number)
+        body = ""
+        for i, message in enumerate(messages):
+            body += "({}/{}) {}\n".format(i, len(messages), message[0])
     else:
         body = ("Welcome to Udacitext.\n"
-                "Send NOW to get the latest announcements! "
-                "To unsubscribe, send STOP. "
-                "(To come back, send START.) "
+                "Text NOW to get the latest announcements! "
+                "To unsubscribe, text STOP. "
+                "(To come back, text START.) "
                 "Happy coding!")
-        try:
-            client = TwilioRestClient(TWILIO_SID, TWILIO_TOKEN)
-            message = client.messages.create(to=phone_number,
-                                             from_=TWILIO_NUMBER,
-                                             body=body)
-        except TwilioRestException as error:
-            with psycopg2.connect(**POSTGRES_KWARGS) as connection:
-                insert_error(connection, error, phone_number)
 
-    return "Udacitext!"
+    response = twilio.twiml.Response()
+    response.message(body)
+    return str(response)
 
 
 def execute_query(connection, return_results, query, *subs):
@@ -77,18 +57,6 @@ def execute_query(connection, return_results, query, *subs):
     results = cursor.fetchall() if return_results else None
     cursor.close()
     return results
-
-
-def insert_error(connection, error, phone_number=None):
-    execute_query(
-        connection, False,
-        """
-        INSERT INTO error_log
-        VALUES (%s, %s, %s, %s, %s, %s, DEFAULT);
-        """,
-        (phone_number, error.status, error.uri, error.method, error.code,
-         error.msg)
-    )
 
 
 if __name__ == "__main__":
